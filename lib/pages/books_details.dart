b6c1e120e8ab1cd4d43f3b61f3921dc37de5dfdf
lucky khanyile <luckykhanyile@gmail.com>
2024-11-04 03:41:39 +0200

    Detials screen half done

--- lib/pages/books_details.dart
+++ lib/pages/books_details.dart
@@ -11,10 +11,31 @@ class BookDetailsScreen extends StatefulWidget {
 class _BookDetailsScreenState extends State<BookDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context).textTheme;
     final args = ModalRoute.of(context)?.settings.arguments as BookDetailsArgs;
     final book = args.itemBook;
     return  Scaffold(
       appBar:  AppBar(title:  Text(book.title)),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              if(book.imageLinks.isNotEmpty) Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Image.network(book.imageLinks['smallThumbnail']??'',
+                fit: BoxFit.cover,
+                ),
+              ),
+              Text(book.title, style: theme.headlineSmall,),
+              Text(book.authors.join(', '), style: theme.bodySmall,),
+              Text('Punlished: ${book.publishedDate}', style: theme.bodySmall,),
+              Text('Page count: ${book.pageCount}', style: theme.bodySmall,),
+              Text('Language: ${book.language}', style: theme.bodySmall,),
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
